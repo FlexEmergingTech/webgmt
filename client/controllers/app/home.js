e84fa6f4709e4bab42c4b32b701b4301eccab2a5
Balamurugan Sivakumar <balamurugan.sivakumar@flex.com>
2018-02-12 01:27:59 -0800

    [Studio] Modified screen "home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -1,5 +1,8 @@
 angular.module('app').controller('app_home', app_home);
 function app_home($scope, app) {
     'use strict';
-    app.init($scope);
+    app.init($scope,function(data){
+        console.log(data);
+    });
+    
 }
