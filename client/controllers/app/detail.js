3a56913f4a524690f6be27233a9a9b8e840ca784
Balamurugan Sivakumar <balamurugan.sivakumar@flex.com>
2018-02-14 03:31:34 -0800

    [Studio] Modified screen "detail"

--- client/controllers/app/detail.js
+++ client/controllers/app/detail.js
@@ -1,11 +1,12 @@
 angular.module('app').controller('app_detail', app_detail);
 function app_detail($scope, app) {
     'use strict';
+    $scope.statusSelect = "Closed";
   app.init($scope,function(data){
       $scope.status = ["OverDue","Closed"];
         console.log(data);
     });
    $scope.update = function(){
-       alert($scope.);
+       alert($scope.statusSelect);
    }
 }
