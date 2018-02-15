f42c81362a905bc40d8a1bff1845516832ea36dd
Balamurugan Sivakumar <balamurugan.sivakumar@flex.com>
2018-02-15 00:36:57 -0800

    [Studio] Modified screen "detail"

--- client/controllers/app/detail.js
+++ client/controllers/app/detail.js
@@ -11,6 +11,6 @@ function app_detail($scope, app) {
         console.log(data);
     });
    $scope.update = function(){
-       alert($scope.statusSelect.label);
+       alert($scope.statusSelect);
    }
 }
