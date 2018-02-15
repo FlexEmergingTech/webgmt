34f9674f54cd4922d1656d6a661141a523780e49
Balamurugan Sivakumar <balamurugan.sivakumar@flex.com>
2018-02-15 05:48:47 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,8 +1,13 @@
 angular.module('app').controller('app_login', app_login);
 function app_login($scope, app, $ionicLoading) {
     'use strict';
-    app.init($scope);
+    
+    app.init($scope,function(data){
+        $scope.crediantialsObj = {};
+    });
    $scope.login = function(){
+       console.log($scope.data.username,$scope.data.password);
+       $scope.crediantialsObj = { "username" : $scope.data.username, "password":$scope.data.password}
        app.call('home.login');
    }
 }
