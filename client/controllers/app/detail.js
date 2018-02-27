37e4d642332ba55cebb8a78038ece83dc4c94655
Balamurugan Sivakumar <balamurugan.sivakumar@flex.com>
2018-02-27 03:22:23 -0800

    [Studio] Modified screen "detail"

--- client/controllers/app/detail.js
+++ client/controllers/app/detail.js
@@ -15,8 +15,8 @@ function app_detail($scope, app) {
           {id:"1",label:"Closed"},
           {id:"2",label:"Open"}];
        // console.log($scope.taskId);
-        $scope.object.statusSelect = $scope.data.status;
-       alert($scope.data.status);
+        //$scope.object.statusSelect = $scope.data.status;
+      // alert($scope.data.status);
     });
    $scope.update = function(){
       console.log($scope.data.comment);
