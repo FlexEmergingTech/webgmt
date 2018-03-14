8307579a99f1663961daff20d5478acf83cf5642
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-03-14 05:05:07 -0700

    [Studio] Modified screen "webgmt"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -2,3 +2,9 @@ angular.module('app').controller('app', app);
 function app($scope, app) {
     'use strict';
 }
+
+app.loginScreen = 'app.login';
+app.loginModel = 'login';
+app.loginAction = 'login';
+app.loginErrorField = 'errorMessage';
+app.loginIsAction = false;
