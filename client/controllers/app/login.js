c8d6ffffda381a76d0f9f939059994fc426a7241
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-03-21 22:44:56 -0700

    [Studio] Modified screen "webgmt", Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -8,7 +8,7 @@ function app_login($scope, $window,app, $ionicLoading) {
    $scope.login = function(){
        
        $scope.crediantialsObj = { "username" : $scope.data.username, "password":$scope.data.password}
-      $window.localStorage.setItem("username",$scope.data.username);
+       $window.localStorage.setItem("username",$scope.data.username);
        app.call('login.login',$scope.crediantialsObj);
    }
 }
