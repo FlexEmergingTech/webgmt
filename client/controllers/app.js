e7e5050473fca8f24a0582bc410c67f66c4d3d0d
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-03-22 06:38:31 -0700

    [Studio] Modified screen "webgmt"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -1,6 +1,15 @@
 angular.module('app').controller('app', app);
 function app($scope, app) {
     'use strict';
+    
+        if (window.isPlatform && window.isPlatform.powWow() && window.isPlatform.iOS() && window.cordova && window.cordova.plugins) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        }
+
+        if (window.screen && window.screen.orientation) {
+            screen.orientation.lock('portrait');
+        }
+    
     app.loginScreen = 'app.login';
     app.loginModel = 'login';
     app.loginAction = 'login';
