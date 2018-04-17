7ed07832d4d9e9b0119359deecfbf3cfd4ec9d8d
Hariharasudhan <Hariharasudhan.M@flex.com>
2018-04-17 02:18:00 -0700

    [Studio] Modified screen "home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -2,13 +2,10 @@ angular.module('app').controller('app_home', app_home);
 function app_home($scope,$window, app) {
     'use strict';
     app.init($scope,function(data){
-        console.log("data",$scope.data.list);
-                console.log("user name: ",$window.localStorage.getItem('username'));
           var username = $window.localStorage.getItem('username');
           $scope.listObj = {};
     });
         $scope.task = function (item) {
-            console.log("item",item);
         $scope.listObj = {
             'username': $window.localStorage.getItem('username'),
             'item': item
