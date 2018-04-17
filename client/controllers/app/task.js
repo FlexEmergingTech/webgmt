49ab37c335ee8304d67089acae8396d7450ac99a
Hariharasudhan <Hariharasudhan.M@flex.com>
2018-04-16 21:48:20 -0700

    [Studio] Add screen "task"

---
+++ client/controllers/app/task.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_task', app_task);
+function app_task($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -69,5 +69,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.task', {
+        views: {
+            app: {
+                controller: 'app_task',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.task');
+                }
+            }
+        }
     });
 });
