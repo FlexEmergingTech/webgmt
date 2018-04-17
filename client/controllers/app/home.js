e4b424b0e2327608ae029edd26e22e3a0a38fcb7
Hariharasudhan <Hariharasudhan.M@flex.com>
2018-04-16 22:09:01 -0700

    [Studio] Modified screen "home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -3,8 +3,15 @@ function app_home($scope,$window, app) {
     'use strict';
     app.init($scope,function(data){
         console.log("data",$scope.data);
-        
+          $scope.crediantialsObj = {};
     });
-    
+    $scope.login = function () {
+        $scope.crediantialsObj = {
+            'username': $scope.data.username,
+            'password': $scope.data.password
+        };
+        $window.localStorage.setItem('username', $scope.data.username);
+        app.call('login.login', $scope.crediantialsObj);
+    };
     
 }
