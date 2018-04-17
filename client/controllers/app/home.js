4cdfc9d09ada1e2f2ff1ebd943509a7b21b5d58d
Hariharasudhan <Hariharasudhan.M@flex.com>
2018-04-17 01:02:10 -0700

    [Studio] Modified screen "home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -5,16 +5,8 @@ function app_home($scope,$window, app) {
         console.log("data",$scope.data.list);
                 console.log("user name: ",$window.localStorage.getItem('username'));
           var username = $window.localStorage.getItem('username');
-          $scope.listObj = {};
+
     });
-        $scope.task = function () {
-            console.log("item",$scope.item);
-        $scope.listObj = {
-            'username': $window.localStorage.getItem('username')
-         
-        };
-        $window.localStorage.setItem('username', $scope.data.username);
-        app.call('login.taskList', $scope.listObj,item);
-    };
+
     
 }
